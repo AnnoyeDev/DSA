680feee267ba105d636b41a9edbabd976818590a
Dev_Pareek <dev_p@ar.iitr.ac.in>
2026-05-27 06:32:00 +0530

    CANDY

--- Question.cpp
+++ Question.cpp
@@ -19,3 +19,27 @@ public:
     return ans;
     }
 };
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+
+        int n = ratings.size();
+        vector<int>left(n);
+        left[0]=1;
+        int sum =0;
+
+        for(int i=1;i<n;i++){
+            if(ratings[i]>ratings[i-1]) left[i] = left[i-1]+1;
+            else left[i]=1;
+        }
+
+        for(int i=n-2;i>=0;i--){
+            if(ratings[i]>ratings[i+1]) left[i] = max(left[i],left[i+1]+1);
+        }
+
+        for(int i=0;i<n;i++){
+            sum =sum +(left[i]);
+        }
+        return sum;
+    }
+};
