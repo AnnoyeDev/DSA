fbe4170a918e19414a16c212433f854a7464b1ab
Dev_Pareek <dev_p@ar.iitr.ac.in>
2026-05-30 04:52:07 +0530

    numberOfSubstrings-Good question

--- Question.cpp
+++ Question.cpp
@@ -135,3 +135,23 @@ class Solution {
         return maxlen;
     }
 };
+class Solution {
+public:
+    int numberOfSubstrings(string s) {
+        int n = s.size();
+        int left=0;
+        int right=0;
+        int cnt=0;
+        vector<int>lastseen(3,-1);
+
+        for(int i =0;i<n;i++){
+            char ch = s[i];
+            lastseen[s[i]-'a']=i;
+
+            if(lastseen[0]!=-1 && lastseen[1]!=-1 && lastseen[2]!=-1){
+                cnt+=(1+min(lastseen[0],min(lastseen[1],lastseen[2])));
+            }
+        }
+        return cnt;        
+    }
+};
