866e429c831f3e9f02e0c0728d1e841306c7fa24
Dev_Pareek <dev_p@ar.iitr.ac.in>
2026-05-31 03:27:30 +0530

    subarraySum

--- Question.cpp
+++ Question.cpp
@@ -197,3 +197,24 @@ public:
         return maxlen;      
     }
 };
+class Solution {
+    public:
+    int subarraySum(vector<int>& nums, int k) {
+    map<int,int>mpp;
+    int n =nums.size();
+    int presum=0;
+    int rem =0;
+    int cnt=0;
+    
+    mpp[0]=1;
+    for(int i =0;i<n;i++){
+        presum+=nums[i];
+        
+        rem = presum -k;
+        if (mpp.find(rem) != mpp.end()) cnt+=mpp[rem];
+        mpp[presum]++;
+
+    }
+    return cnt;
+    }            
+};
